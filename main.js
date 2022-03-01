500171d691d9d3fe3d95f9296f30f9a1fcf25b70
six-6 <1195132772@qq.com>
2022-03-01 21:56:27 +0800

    增加:实现所有功能

--- main.js
+++ main.js
@@ -2,8 +2,48 @@ var __webpack_modules__ = ({
 "./index.js": ((module, __unused_webpack_exports, __webpack_require__) => {
 const { Extension, type, api } = __webpack_require__("clipcc-extension");
 // --- index.js 开始 ---
-class TC_touch extends Extension {
+var stage;
+var touches = [];
+var touchid = {};
+var touchLastid = 1;
+var newTouch = false;
+function O_I(x){
+	return x > 1 ? 1 : x < 0 ? 0 : x;
+}
+function viewLeft(element){
+	return element.getBoundingClientRect().left;
+}
+function viewTop(element){
+	return element.getBoundingClientRect().top;
+}
+function updateTouch(event){
+	try{
+	var list = event.targetTouches;
+	var vaild = {};
+	touches = [];
+	for(var i=0;i<list.length;i++){
+		var touch = list[i];
+		if(!(touch.identifier in touchid)){
+			newTouch = true;
+			touchid[touch.identifier] = touchLastid;
+			touchLastid++;
+		}
+		vaild[touch.identifier] = true;
+		touches.push({
+			id: touchid[touch.identifier],
+			x: Math.floor(-239.5 + 480 * O_I((touch.clientX - viewLeft(stage)) / stage.clientWidth)),
+			y: Math.floor(179.5 - 360 * O_I((touch.clientY - viewTop(stage)) / stage.clientHeight))
+		});
+	}
+	for(var i in touchid){
+		if(!vaild[i]){
+			delete touchid[i];
+		}
+	}
+	}catch(e){alert(e.message)}
+}
 
+class TC_touch extends Extension {
 	onInit() {
 		api.addCategory({
 			categoryId: "touchScreen",
@@ -16,7 +56,9 @@ class TC_touch extends Extension {
 			type: type.BlockType.REPORTER,
 			messageId: "touchScreen.firstpoint",
 			categoryId: "touchScreen",
-			function: function(){return '还在制作';},
+			function: function(){
+				return touches.length !== 0 ? touches[0].id : 0;
+			},
 			param: {}
 		});
 		
@@ -25,7 +67,15 @@ class TC_touch extends Extension {
 			type: type.BlockType.REPORTER,
 			messageId: "touchScreen.nextpoint",
 			categoryId: "touchScreen",
-			function: function(){return '还在制作';},
+			function: function(args){
+				for(var i=0;i<touches.length;i++){
+					if(touches[i].id===Number(args.PREV)){
+						return i===touches.length-1 ?
+							0 : touches[i+1].id;
+					}
+				}
+				return 0;
+			},
 			param: {
 				PREV: {
 					type: type.ParameterType.NUMBER,
@@ -36,10 +86,15 @@ class TC_touch extends Extension {
 		
 		api.addBlock({
 			opcode: "resetindex",
-			type: type.BlockType.BUTTON,
+			type: type.BlockType.COMMAND,
 			messageId: "touchScreen.resetindex",
 			categoryId: "touchScreen",
-			function: function(){alert("还在制作")},
+			function: function(){
+				touches = [];
+				touchid = {};
+				touchLastid = 1;
+				newTouch = false;
+			},
 			param: {}
 		});
 		
@@ -48,7 +103,14 @@ class TC_touch extends Extension {
 			type: type.BlockType.REPORTER,
 			messageId: "touchScreen.touchx",
 			categoryId: "touchScreen",
-			function: function(){return '还在制作';},
+			function: function(args){
+				for(var i=0;i<touches.length;i++){
+					if(touches[i].id===Number(args.POINT)){
+						return touches[i].x;
+					}
+				}
+				return 0;
+			},
 			param: {
 				POINT: {
 					type: type.ParameterType.NUMBER,
@@ -62,7 +124,14 @@ class TC_touch extends Extension {
 			type: type.BlockType.REPORTER,
 			messageId: "touchScreen.touchy",
 			categoryId: "touchScreen",
-			function: function(){return '还在制作';},
+			function: function(args){
+				for(var i=0;i<touches.length;i++){
+					if(touches[i].id===Number(args.POINT)){
+						return touches[i].y;
+					}
+				}
+				return 0;
+			},
 			param: {
 				POINT: {
 					type: type.ParameterType.NUMBER,
@@ -76,7 +145,11 @@ class TC_touch extends Extension {
 			type: type.BlockType.HAT,
 			messageId: "touchScreen.whentouchthestage",
 			categoryId: "touchScreen",
-			function: function(){return false;},
+			function: function(){
+				var value = newTouch;
+				newTouch = false;
+				return value;
+			},
 			param: {}
 		});
 
@@ -85,7 +158,7 @@ class TC_touch extends Extension {
 			type: type.BlockType.BOOLEAN,
 			messageId: "touchScreen.hastouchpoint",
 			categoryId: "touchScreen",
-			function: function(){return false;},
+			function: function(){return touches.length !== 0;},
 			param: {}
 		});
 
@@ -94,10 +167,20 @@ class TC_touch extends Extension {
 			type: type.BlockType.REPORTER,
 			messageId: "touchScreen.countpoint",
 			categoryId: "touchScreen",
-			function: function(){return '还在制作';},
+			function: function(){return touches.length;},
 			param: {}
 		});
 
+		stage = document.querySelector("*[class*=stage_stage_]");
+		if(!stage){
+			alert("无法加载此插件");
+		}
+		stage.addEventListener('touchstart',updateTouch);
+		stage.addEventListener('touchmove',updateTouch);
+		stage.addEventListener('touchend',updateTouch);
+		stage.addEventListener('touchcancel',updateTouch);
+		
+		
 		/* api.addBlock({
 			opcode: '...',
 			type: type.BlockType.xxx,
